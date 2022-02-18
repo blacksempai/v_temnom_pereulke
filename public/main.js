cdf97286f1a809c62748927226006cc37bb314df
blacksempai <benjaminfranclin1998@gmail.com>
2022-02-18 16:36:39 +0200

    add pay

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
 if (document.cookie.includes("token")) {
   let user = localStorage.getItem("user");
   document.getElementById("profile").innerHTML = ` 
-                          <a href='/user'>${user}</a>
-                          <a href='/logout'>LOgout</a>
+                          <p style="font-size: 2.5rem;"><a href='/user'>Profile ${user}</a></p>
+                          <p style="font-size: 2.5rem;"><a href='/logout'>Logout</a></p>
              `;
              console.log(document.getElementsByClassName("auth"));
   for (let a of document.getElementsByClassName("auth")) {
