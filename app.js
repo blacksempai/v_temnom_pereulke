7f90e65189877ed5631824daa832c4d696f04c06
blacksempai <benjaminfranclin1998@gmail.com>
2022-02-18 15:23:20 +0200

    add pay

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const sha1 = require('js-sha1');
 const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 const con = require('./config');
-const fetch = require('node-fetch');
 const multer = require('multer');
 const res = require('express/lib/response');
 const app = express();
@@ -222,3 +221,7 @@ app.get('/cart/products', (req,res)=>{
         }
     });
 })
+
+process.on('uncaughtException', err => {
+    console.error(err && err.stack)
+});
